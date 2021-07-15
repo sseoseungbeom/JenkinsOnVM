c47ec47788a7588b9556a6525f3604f5e41f6a97
sseoseungbeom <seungbami21@gmail.com>
2021-07-15 13:18:53 +0900

    6th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like  twice!');
+    res.send('I like  소녀시대!');
 });
 
 app.get('/start', function(req, res){
