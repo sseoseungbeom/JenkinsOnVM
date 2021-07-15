6d9efb5719e0221abd858ac7ed068a6c517dd770
sseoseungbeom <seungbami21@gmail.com>
2021-07-15 11:48:15 +0900

    2nd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Seungbeom!');
+    res.send('I like  Seo Seungbeom!');
 });
 
 app.get('/start', function(req, res){
